159ee5713819144184c68a93d52f590df5cc0c5c
Arya Ekananda <147296239+AryaEkanandaa@users.noreply.github.com>
2024-05-01 20:50:59 +0800

    Update Restaurants.java

--- PBO-Makanan Online/src/data/Restaurants.java
+++ PBO-Makanan Online/src/data/Restaurants.java
@@ -1,4 +1,10 @@
 package data;
+import entity.Restaurant;
+import java.util.ArrayList;
 
 public class Restaurants {
+    private static ArrayList<Restaurant>restaurants = new ArrayList<Restaurant>();
+    public static ArrayList<Restaurant> getListRestaurant(){
+        return restaurants;
+    }
 }
