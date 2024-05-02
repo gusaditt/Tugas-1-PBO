805ce58534774bff3bf15c749abc5bd479339973
gusaditt <146801690+gusaditt@users.noreply.github.com>
2024-05-02 22:19:27 +0800

    Update Input.java

--- PBO-Makanan Online/src/utility/Input.java
+++ PBO-Makanan Online/src/utility/Input.java
@@ -37,4 +37,31 @@ public class Input {
         sc.nextLine();
         return number;
     }
+
+
+    public static int inputInt(String message,int lowerLimit, int upperLimit){
+        int number;
+        while (true){
+            System.out.println(message);
+            System.out.print("|| ");
+            while (!sc.hasNextInt()) {
+                System.err.println("Masukkan hanya angka");
+                sc.next();
+            }
+            number = sc.nextInt();
+
+
+            if (number >= lowerLimit && number <= upperLimit) {
+                break;
+            }
+            System.err.printf("Pilihan tidak ada ");
+        }
+        sc.nextLine();
+        return number;
+    }
+    public static String inputString(String message){
+        System.out.println(message);
+        System.out.print("|| ");
+        return sc.nextLine();
+    }
 }
