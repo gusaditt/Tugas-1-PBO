c4ca8785da9b3094072b74ffbf8b546ef8a5d098
Arya Ekananda <147296239+AryaEkanandaa@users.noreply.github.com>
2024-05-02 21:49:16 +0800

    wrong file, menu admin

--- PBO-Makanan Online/src/menu/AdminMenu.java
+++ PBO-Makanan Online/src/menu/AdminMenu.java
@@ -1,70 +1,95 @@
 package menu;
 
-import entity.Order;
 import entity.Restaurant;
 import static utility.Input.*;
+import static utility.console.*;
 import static data.Restaurants.*;
-import java.util.ArrayList;
-import static utility.console.clrscr;
-class CusMenu {
-    private static ArrayList<Order>orders = new ArrayList<>();
+
+class AdminMenu {
     public static void main(String[] args){
-        clrscr();
         System.out.println("=============================================");
-        System.out.println("||              Menu Customer              ||");
+        System.out.println("||                Menu Admin               ||");
         System.out.println("=============================================");
-        System.out.println("[1]Buat Pesanan");
-        System.out.println("[2]Lihat Pesanan");
-        System.out.println("[3]Log Out");
-        switch (inputInt("Masukan Pilihan",1,3)){
-            case 1:
-                buatPesanan();
+        System.out.println("[1] Lihat Restaurant");
+        System.out.println("[2] Tambah Restaurant");
+        System.out.println("[3] Hapus Restaurant");
+        System.out.println("[4] Log Out");
+        switch (inputInt("Masukan Pilihan",1,4)){
+            case 1 :
+                lihatRestaurant();
                 break;
-            case 2:
-                lihatPesanan();
+            case 2 :
+                tambahRestaurant();
                 break;
-            case 3:
+            case 3 :
+                hapusRestaurant();
+                break;
+            case 4 :
                 if (inputInt("anda yakin?\n[1]Ya\n[0]Tidak",0,1) == 1){
                     Login.main(null);
                 }else {
                     main(null);
                 }
+            default:
+                System.out.println("menu tidak tersedia");
         }
-    }
-    private static void buatPesanan(){
-        clrscr();
-        System.out.println("=============================================");
-        System.out.println("||              Menu Customer              ||");
-        System.out.println("||              Buat Pesanan               ||");
-        System.out.println("=============================================");
-        if (getListRestaurant().size() == 0){
 
-            System.out.println("Tidak ada Restaurant!");
-            inputInt("[1]Kembali",1,1);
-            main(null);
-        }else {
+    }
+    private static void showListRestaurant(){
+        if (getListRestaurant().size() != 0){
             for (int i = 0; i < getListRestaurant().size(); i++) {
                 System.out.printf("%d.\t",i+1);
-                getListRestaurant().get(i).showData();
-                System.out.println(" ");
+                getListRestaurant().get(i).showAllData();
             }
-            int idRestaurantSelected = inputInt("Pilih Restaurant\n[0]Kembali",0,getListRestaurant().size()) - 1;
-            if (idRestaurantSelected == -1){//kembali ke menu
+        }else {
+            System.out.println("Kosong !");
+        }
+    }
+
+    private static void lihatRestaurant(){
+        System.out.println("=============================================");
+        System.out.println("||             Daftar Restaurant           ||");
+        System.out.println("=============================================");
+
+        showListRestaurant();
+        inputInt("[1]kembali",1,1);
+        main(null);
+    }
+    private static void tambahRestaurant(){
+        do {
+            System.out.println("=============================================");
+            System.out.println("||             Tambah Restaurant           ||");
+            System.out.println("=============================================");
+
+            getListRestaurant().add(new Restaurant(inputString("Nama Restaurant"),inputString("Alamat Restaurant ")));
+            if (inputInt("|| Tambahkan Menu?\n|| [1]Ya\n || [0]Tidak",0,1) == 1){
+                addMenuRestaurant(getListRestaurant().size()-1);//restaurant yang baru ditambahkan pasti berada di index terakir
+            }
+        } while (inputInt("|| Ingin Menambah Restaurant Lagi?\n|| [1]Ya\n|| [0]Tidak", 0, 1) == 1);
+        main(null);
+    }
+    private static void hapusRestaurant(){
+        System.out.println("=============================================");
+        System.out.println("||             Hapus Restaurant            ||");
+        System.out.println("=============================================");
+
+        showListRestaurant();
+        if (getListRestaurant().size() == 0){
+            inputInt("[1]kembali",1,1);
+            main(null);
+        }else {
+            int indexRestaurant = inputInt("Restaurant yang ingin dihapus\n[0]Kembali",0,getListRestaurant().size());
+            if (indexRestaurant == 0){
                 main(null);
-            }if (getListRestaurant().get(idRestaurantSelected).getTotalMenu() == 0){//mengecek apakah terdapat menu di restaurant terpilih
-                System.out.println("Restaurant ini belum memiliki menu!");
-                inputInt("[1]Kembali",1,1);
-                buatPesanan();
-            }else {
-                int jarakAntar = inputInt("Masukan jarak antar lokasi anda",0);
-                orders.add(new Order(idRestaurantSelected,jarakAntar));
-                addMenuOrder(idRestaurantSelected,orders.size()-1);
-                editMenuOrder(idRestaurantSelected,orders.size()-1);
-                if (inputInt("Apakah ingin membuat Pesanan Lagi?\n[1]Ya\n[0]Tidak",0,1) == 1){
-                    buatPesanan();
-                }else {
-                    main(null);
-                }
             }
+            switch (inputInt("apakah anda yakin?\n[1]Ya\n[0]Tidak",0,1)) {
+                case 1:
+                    getListRestaurant().remove(indexRestaurant - 1);
+                    break;
+                case 0:
+                    hapusRestaurant();
+                    break;
+            }
+            main(null);
         }
     }
